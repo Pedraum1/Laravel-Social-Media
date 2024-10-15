55188481b32d49a546e52f854a64e4a363aaac4d
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2024-10-15 20:15:46 -0300

    FIX: Fixing fvicon link

--- resources/views/layouts/layout_base.blade.php
+++ resources/views/layouts/layout_base.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   {{-- favicon --}}
-  <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon">
+  <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
   {{-- vite css/js --}}
   @vite(['resources/css/app.css','resources/js/app.js'])
