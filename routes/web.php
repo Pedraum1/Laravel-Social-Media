9f3a0b413e12b5fcb56f0e78df069094be3cc53f
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2024-10-15 19:55:22 -0300

    FEAT: Creating first routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,16 @@
 <?php
 
+use App\Http\Controllers\Main;
+use App\Http\Middleware\NotVerified;
+use App\Http\Middleware\Verified;
 use Illuminate\Support\Facades\Route;
+
+//Not authenticated routes
+Route::middleware(Verified::class)->group(function(){
+    Route::get('/home',[Main::class,'home'])->name('home');
+});
+
+//Authenticated routes
+Route::middleware(NotVerified::class)->group(function(){
+    Route::view('/','index')->name('index');
+});
