9dec7573d9517ec26d127ad668bb73b1f345b23b
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2024-10-29 20:16:31 -0300

    FEAT: Encryption functions added

---
+++ app/Classes/Encryption.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Classes;
+
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Contracts\Encryption\EncryptException;
+use Illuminate\Support\Facades\Crypt;
+
+class Encryption{
+
+  public static function encryptId(int $id): string|null
+  {
+      try {
+          $id = Crypt::encrypt($id);
+      } catch (EncryptException $e) {
+          return null;
+      }
+      return $id;
+  }
+
+  public static function decryptId(string $id): int|null
+    {
+        try {
+            $id = Crypt::decrypt($id);
+        } catch (DecryptException $e) {
+            return null;
+        }
+        return $id;
+    }
+}
