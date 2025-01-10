78325c7ca443fee4632cb7c0a100c321ee2c66d2
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-01-10 19:34:01 -0300

    STYLE: Grouping routes without sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,23 @@
 
 use App\Classes\API;
 use App\Http\Controllers\Api\AuthApiController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 
 Route::get('/status', function(){
     return API::success(["message"=>"API is running"]);
+})->middleware('auth:sanctum');
+
+Route::controller(AuthApiController::class)->group(function(){
+
+    Route::post('/login','login');
+    Route::post('/register','register');
+    
+    Route::patch('/validate/{token}','validateEmail')
+         ->name('email_validation_with_api');
+    
+    Route::post('/require_password_reset','requirePasswordReset');
+    Route::patch('/reset_password/{token}','resetPassword');
+
 });
 
-Route::post('/login',[AuthApiController::class,'login']);
-Route::post('/register',[AuthApiController::class,'register']);
-Route::post('/logout',[AuthApiController::class,'logout']);
-Route::patch('/validate/{token}',[AuthApiController::class,'validateEmail'])
-     ->name('email_validation_with_api');
-
-Route::post('/require_password_reset',[AuthApiController::class,'requirePasswordReset']);
-Route::patch('/reset_password/{token}',[AuthApiController::class,'resetPassword']);
+Route::post('/logout','logout')->middleware('auth:sanctum');
